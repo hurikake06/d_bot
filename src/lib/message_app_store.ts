08c40bf73441523e4852944f4c189a3970970fd1
hurikake06 <hurikake.oracle@gmail.com>
2021-04-18 11:42:38 +0900

    match_meshi をアプリに追加

--- src/lib/message_app_store.ts
+++ src/lib/message_app_store.ts
@@ -3,13 +3,7 @@ import { MessageApp, LineTextMessage } from '../@types'
 
 export default class MessageAppStore {
   // mode管理対象
-  static modeNames: string[] = ['ModifierGame', 'SummonFreakApp']
-
-  static defaultApp: MessageApp = {
-    name: 'default',
-    check: (text: string): boolean => true,
-    run: (text: string): LineTextMessage => MessageAppStore.fetchApp(env('URL/MODIFIER_GAME'), text)
-  }
+  static modeNames: string[] = ['ModifierGame', 'SummonFreakApp', 'MatchMeshi']
 
   static messageApps: MessageApp[] = [
     { name: 'reset',
@@ -40,6 +34,14 @@ export default class MessageAppStore {
       run: (text: string): LineTextMessage => {
         if(!!text.match(/修飾ゲーム/g)) { return { type: 'text', text: '修飾ゲームをスタートします！！' } }
         return MessageAppStore.fetchApp(env('URL/MODIFIER_GAME'), text)
+      }
+    },
+    {
+      name: 'MatchMeshi',
+      check: (text: string): boolean => !!text.match(/まち飯/g),
+      run: (text: string): LineTextMessage => {
+        if(!!text.match(/まち飯/g)) { return { type: 'text', text: 'あなたの町の隠れた名店を検索！！\n検索したいキーワードを入力してね（ex: 中華, パスタ）' } }
+        return MessageAppStore.fetchApp(env('URL/MATCH_MESHI'), text)
       }
     }
   ]
